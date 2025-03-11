69954a2e2333de4624ec4a53b3b3688418afeb4a
alwaysJOne <alwaysjone@naver.com>
2025-03-11 15:51:33 +0900

    .

--- 09_Spring/boot/src/main/java/com/kh/boot/service/MemberService.java
+++ 09_Spring/boot/src/main/java/com/kh/boot/service/MemberService.java
@@ -5,4 +5,6 @@ import com.kh.boot.domain.vo.Member;
 public interface MemberService {
     //로그인
     Member loginMember(Member m);
+    //회원가입
+    int insertMember(Member m);
 }
--- 09_Spring/boot/src/main/java/com/kh/boot/service/MemberServiceImpl.java
+++ 09_Spring/boot/src/main/java/com/kh/boot/service/MemberServiceImpl.java
@@ -1,13 +1,29 @@
 package com.kh.boot.service;
 
 import com.kh.boot.domain.vo.Member;
+import com.kh.boot.mappers.MemberMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service //@Component보다 더 구체화해서 service객체에 알맞게 bean에 등록
 public class MemberServiceImpl implements MemberService {
 
+    private final MemberMapper memberMapper;
+
+    @Autowired
+    public MemberServiceImpl(MemberMapper memberMapper) {
+        this.memberMapper = memberMapper;
+    }
+
     @Override
     public Member loginMember(Member m) {
-        return null;
+        System.out.println(m.getUserId());
+        return memberMapper.loginMember(m);
+    }
+
+    @Override
+    public int insertMember(Member m) {
+        return 0;
     }
 }
