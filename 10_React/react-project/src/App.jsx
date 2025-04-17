6f43e245129ce708de1456c1af41394e726e1783
wonguwon25 <wonguwon25@gmail.com>
2025-04-17 14:53:51 +0900

    .

--- 10_React/react-project/src/App.jsx
+++ 10_React/react-project/src/App.jsx
@@ -1,12 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import JavaScript from './components/JavaScript'
 
 function App() {
   return (
     <>
-      
+      <JavaScript />
     </>
   )
 }
