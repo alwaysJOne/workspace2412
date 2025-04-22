ad4572373946235446dcd864972e6bd2479a241a
wonguwon25 <wonguwon25@gmail.com>
2025-04-22 12:36:07 +0900

    .

---
+++ 10_React/todo-list/src/components/TodoItem.jsx
@@ -0,0 +1,61 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const TodoItemContainer = styled.li`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 6px 12px;
+    border: 1px solid #e2e2e2c8;
+    margin-bottom: 12px;
+    border-radius: 6px;
+`
+
+const TodoContent = styled.div`
+    display: flex;
+    align-items: center;
+    gap: 12px;
+`
+
+const TodoText = styled.span`
+    text-decoration: none; //line-through; 
+    color: inherit; //#118534
+`
+
+const Checkbox = styled.input`
+    width: 18px;
+    height: 18px;
+`
+
+const DeleteButton = styled.button`
+    color: #d62727;
+    background: none;
+    border: none;
+    cursor: pointer;
+    outline: none;
+
+    &:hover{
+        color: #ac2020;
+    }
+`
+
+const TodoItem = ({todo}) => {
+  return (
+    <TodoItemContainer>
+        <TodoContent>
+            <Checkbox 
+                type="checkbox"
+                checked={todo.completed}
+             />
+             <TodoText>
+                {todo.text}
+             </TodoText>
+        </TodoContent>
+        <DeleteButton>
+            삭제
+        </DeleteButton>
+    </TodoItemContainer>
+  )
+}
+
+export default TodoItem
---
+++ 10_React/todo-list/src/components/TodoList.jsx
@@ -0,0 +1,105 @@
+import React, { useState } from 'react'
+import styled from 'styled-components'
+import TodoItem from './TodoItem'
+
+const Container = styled.div`
+    width: 100%;
+    margin: 0 auto;
+    padding: 24px;
+    background: #ffffff;
+    max-width: 500px;
+    border-radius: 12px;
+    box-shadow: 0 4px 6px #e7e7e7;
+`
+
+const Title = styled.h1`
+    font-size: 48px;
+    font-weight: bold;
+    margin-bottom: 24px;
+`
+
+const InputContainer = styled.div`
+    display: flex;
+    margin-bottom: 24px;
+`
+
+const Input = styled.input`
+    flex: 1;
+    padding: 12px;
+    border: 1px solid #e2e2e2;
+    outline: none;
+    border-radius: 4px 0 0 4px;
+
+    &:focus{
+        border-color: #4b7fcc;
+    }
+`
+const AddButton = styled.button`
+    padding: 12px 24px;
+    background: #4b7fcc;
+    color: #ffffff;
+    border: none;
+    border-radius: 0 4px 4px 0;
+    cursor: pointer;
+
+    &:hover{
+        background-color: #4b7fccdd;
+    }
+`
+
+const TodoListContainer = styled.ul`
+    list-style: none;
+`
+
+
+const TodoList = () => {
+    const [newTodo, setNewTodo] = useState('');
+    const [todos, setTodos] = useState([]);
+
+    const addTodo = () => {
+        if(newTodo.trim() === '') return;
+    
+        const todo = {
+            id : Date.now(),
+            text : newTodo,
+            completed : false,
+        }
+
+        setTodos([...todos, todo]);
+        setNewTodo('');
+    }
+
+    const handleKeyDown = (ev) => {
+        if(ev.key === "Enter"){
+            addTodo();
+        }
+    }
+
+    return (
+        <Container>
+            <Title>Todo List</Title>
+            <InputContainer>
+                <Input
+                    type="text"
+                    value={newTodo}
+                    onChange={(e) => setNewTodo(e.target.value)}
+                    onKeyDown={handleKeyDown}
+                    placeholder='할일을 입력하세요.'
+                />
+                <AddButton onClick={addTodo}>
+                    추가
+                </AddButton>
+            </InputContainer>
+            <TodoListContainer>
+                {todos.map(todo => 
+                    <TodoItem
+                      key={todo.id}
+                      todo={todo}
+                    />
+                )}
+            </TodoListContainer>
+        </Container>
+    )
+}
+
+export default TodoList
