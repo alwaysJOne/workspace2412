10cc8a6cbc7651cbd345ec4239da98f8d1ecc384
wonguwon25 <wonguwon25@gmail.com>
2025-04-24 12:43:02 +0900

    .

--- 10_React/react-project/src/App.jsx
+++ 10_React/react-project/src/App.jsx
@@ -2,12 +2,28 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom'
+import NotFound from './pages/NotFound'
+import Profile from './pages/Profile'
+import About from './pages/About'
+import Home from './pages/Home'
 
 function App() {
   return (
-    <>
-      
-    </>
+    <BrowserRouter>
+      <nav style={{marginBottom: 20}}>
+        {/* Link : a태그와 동일한 역할을 하지만 react-router-dom을 활용해 spa방식으로 자연스럽게 화면전횐 */}
+        <Link to="/" style={{marginRight: 12}}>홈</Link>
+        <Link to="/about" style={{marginRight: 12}}>소개</Link>
+        <Link to="/profile/최지원">프로필</Link>
+      </nav>
+      <Routes>
+        <Route path='/' element={<Home />}/>
+        <Route path='/about' element={<About />}/>
+        <Route path='/profile/:username' element={<Profile />}/>
+        <Route path='*' element={<NotFound />}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
