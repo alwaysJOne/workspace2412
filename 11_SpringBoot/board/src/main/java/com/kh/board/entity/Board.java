72ec3364cb6218a29d11c3e49e730cdd9dc75fd1
wonguwon25 <wonguwon25@gmail.com>
2025-05-13 12:51:45 +0900

    .

--- 11_SpringBoot/board/src/main/java/com/kh/board/entity/Board.java
+++ 11_SpringBoot/board/src/main/java/com/kh/board/entity/Board.java
@@ -1,5 +1,10 @@
 package com.kh.board.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import java.time.LocalDateTime;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
@@ -8,21 +13,37 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Builder
-@ToString
+@Entity //이 클래스가 JPA 엔티티다(DB테이블과 맵핑됨)
 public class Board {
-    private Long boardId;
-    private String title;
-    private String contents;
-    private String fileName;
-    private String memberEmail;
-    private LocalDateTime createdAt;
-    private LocalDateTime updatedAt;
 
-    //변경이 필요한 부분만 setter를 작성해준다.
-    public void changeFileName(String fileName) { this.fileName = fileName; }
+    @Id // PK
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //pk값 자동생성(AUTO_INCREMENT방식)
+    @Column(name = "board_id") //DB컬럼명 지정
+    private Long boardId;
+
+    @Column(nullable = false) //NOT NULL 제약조건
+    private String title;
+
+    @Column(nullable = false, columnDefinition = "TEXT") //데이터타입을 TEXT로 저장
+    private String contents;
+
+    @Column(name = "file_name")
+    private String fileName;
+
+//    private String memberEmail;
+
+    @CreationTimestamp //엔티티가 처음저장될 때 자동으로 현재 시간 저장
+    @Column(name = "created_at", updatable = false) //최초생성시간은 변경이 불가하도록
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp //엔티티가 수정될대마다 자동으로 업데이트 시간을 넣어줌
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
 }
