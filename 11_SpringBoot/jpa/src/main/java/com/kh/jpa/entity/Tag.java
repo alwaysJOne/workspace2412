8f6dec44d854fca8e2114a8115d69d5c1871a20b
wonguwon25 <wonguwon25@gmail.com>
2025-05-15 14:07:19 +0900

    .

--- 11_SpringBoot/jpa/src/main/java/com/kh/jpa/entity/Tag.java
+++ 11_SpringBoot/jpa/src/main/java/com/kh/jpa/entity/Tag.java
@@ -1,19 +1,13 @@
 package com.kh.jpa.entity;
 
-import java.util.ArrayList;
-import java.util.List;
+
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
-/**
- * 태그 정보를 저장하는 엔티티 클래스
- * Board 엔티티와 N:M 관계
- */
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
