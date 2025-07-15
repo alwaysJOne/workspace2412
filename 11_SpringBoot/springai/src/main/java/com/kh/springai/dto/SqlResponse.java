c94cddfc6274dbee6152d442d3def031b1e948d7
wonguwon25 <wonguwon25@gmail.com>
2025-07-15 14:07:52 +0900

    .

---
+++ 11_SpringBoot/springai/src/main/java/com/kh/springai/dto/SqlResponse.java
@@ -0,0 +1,15 @@
+package com.kh.springai.dto;
+
+import java.util.List;
+import java.util.Map;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class SqlResponse {
+    private String query;
+    private List<Map<String, Object>> result;
+}
